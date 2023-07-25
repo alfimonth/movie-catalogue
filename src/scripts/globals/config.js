9f24d61d32a071cadb020729be9f068779eee922
alfimonth <alvy1610@gmail.com>
2023-07-25 09:27:08 +0700

    melakukan request ke API

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '6229751a3ece13df681d0cee3b8699a8',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
